6c52cb052cab31c99bf3a6e8c4ebc3a87f0c41fc
Ignacio Garcia <garcia.nunez@tum.de>
2025-01-23 13:39:02 +0100

    add perftest on draft e

--- practice_session2025/e.py
+++ practice_session2025/e.py
@@ -1,3 +1,5 @@
+import random
+import time
 from itertools import combinations_with_replacement
 
 
@@ -10,6 +12,15 @@ def solve():
     out = 0
     for t1, t2 in combinations_with_replacement(c, 2):
         out = max(out, eval_pair(t1, t2))
+    return out
+
+
+def filter_pairs(c):
+    out = []
+    for min_dim, max_dim in c:
+        if not any([min_dim < min_dim2 and max_dim < max_dim2 for min_dim2, max_dim2 in c]):
+            out.append((min_dim, max_dim))
+
     return out
 
 
@@ -35,4 +46,34 @@ c = list(map(
     ])
 )
 
-print(solve_v2(c))
+print(solve_v2(filter_pairs(c)))
+
+"""
+def generate_pairs(n=int(1e4)):
+    out = []
+    for i in range(n):
+        min_dim = random.randint(1, 1000)
+        out.append((min_dim, random.randint(min_dim, 2000)))
+    return out
+
+c = generate_pairs()
+
+start = time.time()
+out1 = solve()
+end = time.time()
+print(f"v1: {end - start}s, {out1=}")
+
+start = time.time()
+out2 = solve_v2(c)
+end = time.time()
+print(f"v2: {end - start}s, {out2=}")
+
+start = time.time()
+out3 = solve_v2(filter_pairs(c))
+end = time.time()
+print(f"v3: {end - start}s, {out3=}")
+
+# v1: 11.597122192382812s, out1=1988000000
+# v2: 14.129420757293701s, out2=1988000000
+# v3: 3.031428098678589s, out3=1988000000
+"""
